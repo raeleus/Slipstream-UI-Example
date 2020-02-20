4c7171f0412f47bf3fd48b1c601000afb637b3dc
Raeleus <raymond.ray3k@gmail.com>
2020-02-19 20:06:20 -0800

    Added Graph simulation of acceleration.

--- core/src/com/ray3k/slipstreamui/Core.java
+++ core/src/com/ray3k/slipstreamui/Core.java
@@ -4,18 +4,16 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.TemporalAction;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import space.earlygrey.shapedrawer.GraphDrawer;
+import space.earlygrey.shapedrawer.ShapeDrawer;
+import space.earlygrey.shapedrawer.scene2d.GraphDrawerDrawable;
 
 public class Core extends ApplicationAdapter {
 	private Skin skin;
@@ -45,10 +43,22 @@ public class Core extends ApplicationAdapter {
 		
 		TextButton textButton = new TextButton("GRAND PRIX", skin, "menu");
 		root.add(textButton);
+		textButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				showVehicleStats();
+			}
+		});
 		
 		root.row();
 		textButton = new TextButton("PRACTICE", skin, "menu-faded");
 		root.add(textButton);
+		textButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				showVehicleStats();
+			}
+		});
 		
 		root.row();
 		textButton = new TextButton("RECORDS", skin, "menu-faded");
@@ -57,7 +67,7 @@ public class Core extends ApplicationAdapter {
 		root.row();
 		textButton = new TextButton("SETTINGS", skin, "menu-faded");
 		root.add(textButton);
-		root.addListener(new ChangeListener() {
+		textButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
 				showSettings();
@@ -113,6 +123,113 @@ public class Core extends ApplicationAdapter {
 		root.row();
 		ImageButton imageButton = new ImageButton(skin, "ok");
 		root.add(imageButton).expandY().bottom();
+		imageButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				showMenu();
+			}
+		});
+	}
+	
+	private void showVehicleStats() {
+		stage.clear();
+		
+		Stack stack = new Stack();
+		stack.setFillParent(true);
+		stage.addActor(stack);
+		
+		Image image = new Image(skin, "black");
+		stack.add(image);
+		
+		Container container = new Container();
+		stack.add(container);
+		
+		image = new Image(skin, "box-ten");
+		container.setActor(image);
+		container.pad(10);
+		container.fill();
+		
+		Table root = new Table();
+		stack.add(root);
+		
+		Label label = new Label("Blue Falcon", skin, "button");
+		root.add(label).expandY().top();
+		
+		root.row();
+		Table table = new Table();
+		root.add(table);
+		
+		table.defaults().space(5).uniformX().left();
+		label = new Label("ENGINE UNIT", skin);
+		table.add(label);
+		
+		label = new Label(": BF-2001x4", skin);
+		table.add(label);
+		
+		table.row();
+		label = new Label("MAX POWER", skin);
+		table.add(label);
+		
+		label = new Label(": 3200ps", skin);
+		table.add(label);
+		
+		table.row();
+		label = new Label("MAX SPEED", skin);
+		table.add(label);
+		
+		label = new Label(": 457km-h", skin);
+		table.add(label);
+		
+		table.row();
+		label = new Label("WEIGHT", skin);
+		table.add(label);
+		
+		label = new Label(": 1260kg", skin);
+		table.add(label);
+		
+		root.row();
+		label = new Label("ACCELERATION", skin);
+		root.add(label).space(10);
+		
+		root.row();
+		stack = new Stack();
+		root.add(stack).size(150, 75);
+		
+		image = new Image(skin, "acceleration-grid-ten");
+		stack.add(image);
+		
+		container = new Container();
+		stack.add(container);
+		
+		final GraphDrawerDrawable graphDrawerDrawable = new GraphDrawerDrawable(new GraphDrawer(new ShapeDrawer(stage.getBatch(), skin.getRegion("white"))));
+		graphDrawerDrawable.setInterpolation(Interpolation.sine);
+		graphDrawerDrawable.setColor(Color.WHITE);
+		graphDrawerDrawable.setDomainEnd(0);
+		image = new Image(graphDrawerDrawable);
+		container.setActor(image);
+		container.fill().padLeft(25).padBottom(10).padTop(20).padRight(5);
+		image.addAction(new TemporalAction(2f) {
+			@Override
+			protected void update(float percent) {
+				graphDrawerDrawable.setDomainEnd(percent);
+			}
+		});
+		
+		root.row();
+		table = new Table();
+		root.add(table).expandY().bottom();
+		
+		ImageButton imageButton = new ImageButton(skin, "yes");
+		table.add(imageButton);
+		imageButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				showMenu();
+			}
+		});
+		
+		imageButton = new ImageButton(skin, "no");
+		table.add(imageButton);
 		imageButton.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
